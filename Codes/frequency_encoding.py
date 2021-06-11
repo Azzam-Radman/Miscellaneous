af66ee60f9f175d978fb23090480bc1819d5b3be
Azzam-Radman <66713429+Azzam-Radman@users.noreply.github.com>
2021-06-11 22:18:44 +0300

    Update frequency_encoding.py

--- Codes/frequency_encoding.py
+++ Codes/frequency_encoding.py
@@ -3,25 +3,27 @@ import numpy as np
 
 def freq_enc(df, cols):
     
+    new_df = df.copy()
+    
     for col in cols:
         
-        counts = pd.DataFrame(train[col].value_counts()).reset_index(drop=False)
+        counts = df.groupby(col)[col].agg(['count']) / len(df)
+        counts = counts.reset_index(drop=False)
         
-        df = df.merge(counts, left_on=[col], 
-                      right_on=['index'], how='left', 
-                      suffixes=('', '_count')).drop('index', axis=1)
+        new_df[col] = df.merge(counts, on=col, how='left', 
+                      suffixes=('', f'{col}_count'))['count']
         
-    return df
+    return new_df
   
 columns_ = [f for f in train.columns if f not in ['target', 'id']]
 
 train_len = len(train)
+#train = train.drop('target', axis=1) # drop the target values first
 full_data = pd.concat([train, test], axis=0)
-full_data = freq_enc(full_data, columns_)
+full_data_enc = freq_enc(full_data, columns_)
 
-train_freq_enc = full_data.iloc[:train_len, :].reset_index(drop=True)
-test_freq_enc = full_data.iloc[train_len:, :].drop('target', axis=1).reset_index(drop=True)
-
+train_freq_enc = full_data_enc.iloc[:train_len, :].reset_index(drop=True)
+test_freq_enc = full_data_enc.iloc[train_len:, :].reset_index(drop=True)
 
 train_freq_enc.to_csv('train_frequecy_encoded.csv', index=False)
 test_freq_enc.to_csv('test_frequecy_encoded.csv', index=False)
