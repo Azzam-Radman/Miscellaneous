b76d2c67ef73932357738f7511207c77b68ff09a
Azzam-Radman <66713429+Azzam-Radman@users.noreply.github.com>
2021-06-12 06:29:16 +0300

    Update frequency_encoding.py

--- Codes/frequency_encoding.py
+++ Codes/frequency_encoding.py
@@ -1,29 +1,31 @@
 import pandas as pd
 import numpy as np
 
+train = pd.read_csv('train.csv')
+test = pd.read_csv('test.csv').drop('id', axis=1)
+sample = pd.read_csv('sample_submission.csv')
+
+cols = [f for f in train.columns if f not in ['id', 'target']]
+X = train[cols]
+len_X = len(X)
+y = train['target']
+full_data = pd.concat([X, test], axis=0).reset_index(drop=False)
+
 def freq_enc(df, cols):
     
-    new_df = df.copy()
-    
     for col in cols:
+        grouped = df.groupby(col)['index'].agg('count') / len(df)
+        grouped = grouped.to_frame().reset_index(drop=False).rename(columns={'index': f'{col}_count'})
+        merged = df.merge(grouped, on=col, how='left').drop('index', axis=1)
         
-        counts = df.groupby(col)[col].agg(['count']) / len(df)
-        counts = counts.reset_index(drop=False)
-        
-        new_df[col] = df.merge(counts, on=col, how='left', 
-                      suffixes=('', f'{col}_count'))['count']
-        
-    return new_df
-  
-columns_ = [f for f in train.columns if f not in ['target', 'id']]
+        df[f"{col}_count"] = merged[f'{col}_count']
+    return df
 
-train_len = len(train)
-#train = train.drop('target', axis=1) # drop the target values first
-full_data = pd.concat([train, test], axis=0)
-full_data_enc = freq_enc(full_data, columns_)
 
-train_freq_enc = full_data_enc.iloc[:train_len, :].reset_index(drop=True)
-test_freq_enc = full_data_enc.iloc[train_len:, :].reset_index(drop=True)
+full_encoded = freq_enc(full_data, cols)
+X_enc = full_encoded.iloc[:len_X, :]
+test_enc = full_encoded.iloc[len_X:, :]
 
-train_freq_enc.to_csv('train_frequecy_encoded.csv', index=False)
-test_freq_enc.to_csv('test_frequecy_encoded.csv', index=False)
+train_enc = pd.concat([X_enc, y], axis=1)
+train_enc.to_csv('train_encoded.csv', index=False)
+test_enc.to_csv('test_encoded.csv', index=False)
