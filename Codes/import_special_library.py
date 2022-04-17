25a074a399bd289d9cf8abda25a7aa5dba8bb320
Azzam-Radman <66713429+Azzam-Radman@users.noreply.github.com>
2022-04-17 17:18:12 +0300

    Create import_special_library.py

---
+++ Codes/import_special_library.py
@@ -0,0 +1,6 @@
+import sys
+sys.path.append('/kaggle/input/efficientnetv2-pretrained-imagenet21k-weights/brain_automl/')
+sys.path.append('/kaggle/input/efficientnetv2-pretrained-imagenet21k-weights/brain_automl/efficientnetv2/')
+
+!pip install keras-efficientnet-v2
+import keras_efficientnet_v
