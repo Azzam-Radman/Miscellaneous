fe90cd129540c135c4cff52b42f59ed6ad7fc75c
Azzam-Radman <66713429+Azzam-Radman@users.noreply.github.com>
2022-04-26 01:19:43 +0300

    Update good_lr_scheduler.py

--- Codes/good_lr_scheduler.py
+++ Codes/good_lr_scheduler.py
@@ -2,5 +2,11 @@ lr_scheduler = tf.keras.callbacks.LearningRateScheduler(lambda epoch: 1e-8 * 10*
 history = model.fit(train_set, epochs=100, callbacks=[lr_scheduler]) # train for 100 epoch then make a plot, visualize the best performing LR and pick it
 
 # then plot the loss versus the learning rate
-plt.semilogx(history.history['lr'], history.history['loss'])
-plt.axis([1e-8, 1e-4, 0, 2])
+plt.figure(figsize=(12, 7))
+plt.semilogx(history.history['lr'], history.history['val_mae'])
+min_lr = history.history['lr'].min()
+max_lr = history.history['lr'].max()
+min_mae = history.history['val_mae'].min()
+max_mae = history.history['val_mae'].max()
+plt.axis([min_lr, max_lr, min_mae, max_mae])
+plt.show()
