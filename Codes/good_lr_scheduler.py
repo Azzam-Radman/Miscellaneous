8d4070e508de37a11f6ece4d0662fa2d06d3b92d
Azzam-Radman <66713429+Azzam-Radman@users.noreply.github.com>
2022-04-26 01:17:29 +0300

    Update good_lr_scheduler.py

--- Codes/good_lr_scheduler.py
+++ Codes/good_lr_scheduler.py
@@ -1,4 +1,4 @@
-lr_scheduler = tf.keras.callbacks.LearningRateScheduler(lambda epoch: 1e-8 * 10**(epoch/20)) # epoch strats from 1 (gradually increase LR to 1e-3)
+lr_scheduler = tf.keras.callbacks.LearningRateScheduler(lambda epoch: 1e-8 * 10**((epoch+1)/20)) # epoch strats from 0 (gradually increase LR to 1e-3)
 history = model.fit(train_set, epochs=100, callbacks=[lr_scheduler]) # train for 100 epoch then make a plot, visualize the best performing LR and pick it
 
 # then plot the loss versus the learning rate
