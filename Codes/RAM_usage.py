d6f89400d829607be36d661bac8dc440f94d07d6
Azzam-Radman <66713429+Azzam-Radman@users.noreply.github.com>
2022-06-24 18:59:32 +0300

    Update RAM_usage.py

--- Codes/RAM_usage.py
+++ Codes/RAM_usage.py
@@ -4,3 +4,8 @@ import psutil
 
 # Process.memory_info is expressed in bytes, so convert to megabytes
 print(f"RAM used: {psutil.Process().memory_info().rss / (1024 * 1024):.2f} MB")
+
+# Here the rss attribute refers to the resident set size, 
+# which is the fraction of memory that a process occupies in RAM. 
+# This measurement also includes the memory used by the Python interpreter and the libraries we’ve loaded, 
+# so the actual amount of memory used to load the dataset is a bit smaller. 
